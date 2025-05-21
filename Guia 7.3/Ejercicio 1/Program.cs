c797508ca87404676428c851ea645951304bb17d
= <=>
2025-05-21 15:44:45 -0300

    Ejercicio 1 y 2

---
+++ Guia 7.3/Ejercicio 1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Calcular el promedio de 20 números ingresados por el usuario.
+
+namespace Ejercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, promedio;
+            int suma = 0;
+
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine("Ingrese un número:");
+                num = Convert.ToInt32(Console.ReadLine());
+                suma += num;
+            }
+
+            promedio = suma / 20;
+
+            Console.WriteLine("El promedio de los 20 números es: " + promedio);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Guia 7.3/Ejercicio 2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+/*Calcular el promedio de una cantidad indeterminada de números, el rango de números ingresados es de 0 a 100.
+El usuario no conoce en ningún momento la cantidad de números a ingresar.*/
+
+namespace Ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            int suma = 0;
+            int contador = 0;
+
+            while (num != -1)
+            {
+                Console.WriteLine("Ingrese un número (0-100) o -1 para salir:");
+                num = Convert.ToInt32(Console.ReadLine());
+                if (num >= 0 && num <= 100)
+                {
+                    suma += num;
+                    contador++;
+                }
+                else if (num != -1)
+                {
+                    Console.WriteLine("Número fuera de rango. Intente nuevamente.");
+                }
+            }
+
+            if (contador > 0)
+            {
+                double promedio = (double)suma / contador;
+                Console.WriteLine($"El promedio de los números ingresados es: {promedio}");
+            }
+            else
+            {
+                Console.WriteLine("No se ingresaron números.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
